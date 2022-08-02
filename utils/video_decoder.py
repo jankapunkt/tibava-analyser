8a4c58b40ca2e5109d6bd52adfac3909433f85d8
eric-mb <e-mb@tutanota.com>
2022-08-02 13:43:32 +0200

    changed video_decoder plugin to PyAV as it (1) more stable according to imageio; (2) fixes an issue of the ffmpeg pluging that frames extracted with low fps seem to have a time delay compared to the video in full fps

--- utils/video_decoder.py
+++ utils/video_decoder.py
@@ -1,16 +1,14 @@
-import imageio
+import imageio.v3 as iio
 
 
 class VideoDecoder:
-    # TODO: test.mp4 with vertical orientation is loaded with horizontal orientation
-    # TODO: it seems that SAR and DAR information provided by ffprobe is not used by imageio
+    # TODO: videos with sample aspect ratio (SAR) not equal to 1:1 are loaded with wrong shape
     def __init__(self, path, max_dimension=None, fps=None):
         self._path = path
         self._max_dimension = max_dimension
         self._fps = fps
-        reader = imageio.get_reader(path)
 
-        self._meta = reader.get_meta_data()
+        self._meta = iio.immeta(path)
         self._size = self._meta.get("size")
 
         if self._fps is None:
@@ -22,9 +20,13 @@ class VideoDecoder:
             res = max(self._size[1], self._size[0])
             scale = min(self._max_dimension / res, 1)
             res = (round(self._size[0] * scale), round(self._size[1] * scale))
-            video_reader = imageio.get_reader(self._path, fps=self._fps, size=res)
+            video_reader = iio.imread(
+                self._path,
+                plugin="pyav",
+                filter_sequence=[("fps", f"{self._fps}"), ("scale", {"width": f"{res[0]}", "height": f"{res[1]}"})],
+            )
         else:
-            video_reader = imageio.get_reader(self._path, fps=self._fps)
+            video_reader = iio.imread(self._path, fps=self._fps, plugin="pyav")
 
         for i, frame in enumerate(video_reader):
             yield {"time": i / self._fps, "index": i, "frame": frame}
