74b52cfdf7aed1e47d91bce8080a151d99d899f4
matthias springstein <matthias.springstein@gmail.com>
2022-10-17 14:15:08 +0000

    Move data path generator to a deeper structure.

--- data.py
+++ data.py
@@ -23,8 +23,8 @@ from analyser.utils import ByteFIFO
 
 
 def create_data_path(data_dir, data_id, file_ext):
-    os.makedirs(data_dir, exist_ok=True)
-    data_path = os.path.join(data_dir, f"{data_id}.{file_ext}")
+    os.makedirs(os.path.join(data_dir, data_id[0:2], data_id[2:4]), exist_ok=True)
+    data_path = os.path.join(data_dir, data_id[0:2], data_id[2:4], f"{data_id}.{file_ext}")
     return data_path
 
 
