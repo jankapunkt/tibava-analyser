149fd8eefe5358cd7024a8d7c17267b784e57062
eric-mb <e-mb@tutanota.com>
2022-12-02 17:03:46 +0100

    script to run a pipeline of plugins with defined paramters based on a .yml definition file

---
+++ scripts/pickle_pipelines.py
@@ -0,0 +1,186 @@
+import argparse
+import hashlib
+import json
+import logging
+import os
+from pathlib import Path
+import pickle
+import yaml
+
+from analyser.client import AnalyserClient
+
+
+def flat_dict(data_dict, parse_json=False):
+    result_map = {}
+    for k, v in data_dict.items():
+        if isinstance(v, dict):
+            embedded = flat_dict(v)
+            for s_k, s_v in embedded.items():
+                s_k = f"{k}.{s_k}"
+                if s_k in result_map:
+                    logging.error(f"flat_dict: {s_k} alread exist in output dict")
+
+                result_map[s_k] = s_v
+            continue
+
+        if k not in result_map:
+            result_map[k] = []
+        result_map[k] = v
+    return result_map
+
+
+def get_hash_for_plugin(plugin, parameters=[], inputs=[]):
+    return hashlib.sha256(
+        json.dumps(
+            flat_dict(
+                {
+                    "plugin": plugin,
+                    "parameters": parameters,
+                    "inputs": inputs,
+                }
+            )
+        ).encode()
+    ).hexdigest()
+
+
+def analyse_video(
+    client: AnalyserClient, video_path: Path, output_path: Path, pipelines: dict, available_plugins: list
+):
+    """Runs all plugins and computes their preconditions if necessary for a given video and an established client.
+    Results will be saved as a pickled dictionary.
+
+    Args:
+        client (AnalyserClient): A client that can communicate with the backend.
+        video_path (Path): The path of the video to be analysed.
+        output_path (Path): The destination for results.
+        pipelines (dict): Pipeline dictionary containing the plugins to run and their parameters.
+        available_plugins (list): List of available analyser plugins.
+    """
+    # Upload Video
+    logging.info("Uploading file...")
+    video = client.upload_file(video_path)  # NOTE must match the input of the pipeline
+    logging.info(f"Done! ID: {video}")
+
+    cache = {}
+    for pipeline in pipelines:
+        logging.info(f"{pipeline['pipeline']}: STARTED!")
+        logging.debug(pipeline)
+
+        data = {"video": video}
+
+        for entry in pipeline["inputs"]:
+            if entry not in data:
+                logging.error("input data missing!")
+
+        for plugin in pipeline["plugins"]:
+            # TODO check if plugin is available
+            # if plugin["plugin"] not in available_plugins:  # TODO
+            #     logging.warning(f"Unknown plugin: {plugin}")
+            #     break
+
+            logging.info(f"{pipeline['pipeline']}/{plugin['plugin']}: RUNNING ...")
+            logging.debug(plugin)
+
+            # create parameters and inputs
+            inputs = []
+            if "requires" in plugin:
+                inputs = [{"name": key, "id": data[value]} for key, value in plugin["requires"].items()]
+
+            parameters = []
+            if "parameters" in plugin:
+                parameters = [{"name": key, "value": value} for key, value in plugin["parameters"].items()]
+
+            # create plugin hash and load results from cache if already computed
+            plugin_hash = get_hash_for_plugin(plugin=plugin["plugin"], parameters=parameters, inputs=inputs)
+            if plugin_hash in cache:
+                data = {**data, **cache[plugin_hash]}
+                logging.info(f"{pipeline['pipeline']}/{plugin['plugin']}: LOADED FROM CACHE!")
+                continue
+
+            # run plugin
+            job_id = client.run_plugin(plugin["plugin"], inputs, parameters)
+            result = client.get_plugin_results(job_id=job_id)
+            if result is None:
+                logging.info(f"{pipeline['pipeline']}/{plugin['plugin']}: No result!")
+                continue
+
+            plugin_results = {}
+            for output in result.outputs:
+                if output.name in plugin["provides"].keys():
+                    plugin_results[plugin["provides"][output.name]] = output.id
+
+            # store plugin results
+            cache[plugin_hash] = plugin_results
+            data = {**data, **plugin_results}
+
+            logging.info(f"{pipeline['pipeline']}/{plugin['plugin']}: DONE!")
+
+        # store results
+        filename = os.path.join(output_path, video, f"{pipeline['pipeline']}.pkl")
+        store_output_as_pkl(filename, data)
+        logging.info(f"{pipeline['pipeline']}: DONE!")
+
+    return True
+
+
+def store_output_as_pkl(filename, data):
+
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+
+    with open(filename, "wb") as f:
+        pickle.dump(data, f)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run analyser plugins and store them in pickled dictionaries.")
+
+    parser.add_argument("-v", "--videos", type=str, nargs="+", help="Path to video file(s)")
+    parser.add_argument("-p", "--pipelines", type=str, help="Path to .yml file defining the pipelines to run")
+    parser.add_argument("-o", "--output_path", type=str, help="Path to the output folder for .pkl files")
+
+    parser.add_argument("--host", default="localhost", help="host name")
+    parser.add_argument("--port", default=50051, help="port number")
+    parser.add_argument("--debug", action="store_true", help="debug output")
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    # load arguments
+    args = parse_args()
+
+    # define logging level and format
+    level = logging.INFO
+    if args.debug:
+        level = logging.DEBUG
+
+    logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=level)
+
+    # read pipeline definitions
+    with open(args.pipelines, "r") as f:
+        pipelines = yaml.safe_load(f)
+
+    # start client and get available plugins
+    client = AnalyserClient(host=args.host, port=args.port)
+    available_plugins = client.list_plugins()  # TODO
+    for plugin in available_plugins:
+        print(plugin)
+    print(available_plugins)
+    return
+    logging.debug(available_plugins)
+
+    # analyse videos
+    for video in args.videos:
+        analyse_video(
+            client=client,
+            video_path=Path(video),
+            output_path=Path(args.output_path),
+            pipelines=pipelines,
+            available_plugins=available_plugins,
+        )
+
+
+if "__main__" == __name__:
+    main()
