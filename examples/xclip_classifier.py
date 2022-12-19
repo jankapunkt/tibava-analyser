105375ad74a90e5558ae4c60b5ea6ab0c3b96143
Jim Rhotert <rhotert@stud.uni-hannover.de>
2022-12-19 17:44:41 +0100

    Add XCLIP plugin

---
+++ examples/xclip_classifier.py
@@ -0,0 +1,54 @@
+import sys
+import argparse
+import logging
+
+from analyser.client import AnalyserClient
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose output")
+    parser.add_argument("--input_path", default="/media/test.webm", help="path to input video .mp4")
+    parser.add_argument("--output_path", default="/media", help="path to output folder")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    level = logging.ERROR
+    if args.verbose:
+        level = print
+
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", datefmt="%d-%m-%Y %H:%M:%S", level=level)
+
+    client = AnalyserClient("localhost", 50051)
+
+    logging.info(f"Start uploading")
+    data_id = client.upload_file(args.input_path)
+    logging.info(f"Upload done: {data_id}")
+
+    job_id = client.run_plugin("xclip_classifier", [{"id": data_id, "name": "video"}], [])
+    logging.info(f"Job xclip_classifier started: {job_id}")
+
+    result = client.get_plugin_results(job_id=job_id)
+
+    if result is None:
+        logging.error("Job is crashing")
+        return
+    logging.info("Plugin results:", result.outputs)
+
+    logits_id = None
+    for output in result.outputs:
+        if output.name == "logits":
+            logits_id = output.id
+
+    logging.info(client.download_data(logits_id, args.output_path))
+
+
+    return 0
+
+if "__main__" == __name__:
+    main()
