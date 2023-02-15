195b5f898012070d122d89e281ba0311345000c6
Matthias Springstein <matthias.springstein@gmail.com>
2023-02-15 10:42:30 +0100

    Change the normalization of the clip probs.

--- analyser/plugin/plugins/clip_embedding.py
+++ analyser/plugin/plugins/clip_embedding.py
@@ -134,6 +134,8 @@ class ClipTextEmbedding(
 
 prob_parameters = {
     "search_term": "",
+    "normalize": True,
+    "softmax": False,
 }
 
 prob_requires = {
@@ -150,7 +152,7 @@ class ClipProbs(
     AnalyserPlugin,
     config=default_config,
     parameters=prob_parameters,
-    version="0.1",
+    version="0.3",
     requires=prob_requires,
     provides=prob_provides,
 ):
@@ -171,10 +173,20 @@ class ClipProbs(
             probs = []
             time = []
             delta_time = None
-            pos_text = parameters["search_term"]
-            result = self.server({"data": pos_text}, ["embedding"])
+            pos_embeddings = []
+            for t in openai_imagenet_template:
+                pos_text = t(parameters["search_term"])
+                result = self.server({"data": pos_text}, ["embedding"])
 
-            text_embedding = normalize(result["embedding"])
+                text_embedding = normalize(result["embedding"])
+                pos_embeddings.append(text_embedding)
+                print(f"################1 {text_embedding.shape}")
+            pos_embeddings = np.concatenate(pos_embeddings, axis=0)
+            print(f"################2 {pos_embeddings.shape}")
+            pos_embeddings = np.mean(pos_embeddings, axis=0, keepdims=True)
+            print(f"################3 {pos_embeddings.shape}")
+            text_embedding = normalize(pos_embeddings)
+            print(f"################4 {text_embedding.shape}")
 
             neg_text = "Not " + parameters["search_term"]
             neg_result = self.server({"data": neg_text}, ["embedding"])
@@ -184,18 +196,108 @@ class ClipProbs(
             text_embedding = np.concatenate([text_embedding, neg_text_embedding], axis=0)
             for embedding in input_data.embeddings:
 
-                result = 100 * text_embedding @ embedding.embedding.T
-
-                prob = scipy.special.softmax(result, axis=0)
+                if parameters["softmax"]:
+                    result = 100 * text_embedding @ embedding.embedding.T
+                    prob = scipy.special.softmax(result, axis=0)
+                else:
+                    prob = text_embedding @ embedding.embedding.T
 
                 # sim = 1 - spatial.distance.cosine(embedding.embedding, text_embedding)
                 probs.append(prob[0, 0])
                 time.append(embedding.time)
                 delta_time = embedding.delta_time
 
+            y = np.array(probs)
+            if parameters["normalize"]:
+                y = (y - np.min(y)) / (np.max(y) - np.min(y))
+
             self.update_callbacks(callbacks, progress=1.0)
-            output_data.y = np.array(probs)
+            output_data.y = y
             output_data.time = time
             output_data.delta_time = delta_time
             output_data.name = "image_text_similarities"
             return {"probs": output_data}
+
+
+openai_imagenet_template = [
+    lambda c: f"a bad photo of a {c}.",
+    lambda c: f"a photo of many {c}.",
+    lambda c: f"a sculpture of a {c}.",
+    lambda c: f"a photo of the hard to see {c}.",
+    lambda c: f"a low resolution photo of the {c}.",
+    lambda c: f"a rendering of a {c}.",
+    lambda c: f"graffiti of a {c}.",
+    lambda c: f"a bad photo of the {c}.",
+    lambda c: f"a cropped photo of the {c}.",
+    lambda c: f"a tattoo of a {c}.",
+    lambda c: f"the embroidered {c}.",
+    lambda c: f"a photo of a hard to see {c}.",
+    lambda c: f"a bright photo of a {c}.",
+    lambda c: f"a photo of a clean {c}.",
+    lambda c: f"a photo of a dirty {c}.",
+    lambda c: f"a dark photo of the {c}.",
+    lambda c: f"a drawing of a {c}.",
+    lambda c: f"a photo of my {c}.",
+    lambda c: f"the plastic {c}.",
+    lambda c: f"a photo of the cool {c}.",
+    lambda c: f"a close-up photo of a {c}.",
+    lambda c: f"a black and white photo of the {c}.",
+    lambda c: f"a painting of the {c}.",
+    lambda c: f"a painting of a {c}.",
+    lambda c: f"a pixelated photo of the {c}.",
+    lambda c: f"a sculpture of the {c}.",
+    lambda c: f"a bright photo of the {c}.",
+    lambda c: f"a cropped photo of a {c}.",
+    lambda c: f"a plastic {c}.",
+    lambda c: f"a photo of the dirty {c}.",
+    lambda c: f"a jpeg corrupted photo of a {c}.",
+    lambda c: f"a blurry photo of the {c}.",
+    lambda c: f"a photo of the {c}.",
+    lambda c: f"a good photo of the {c}.",
+    lambda c: f"a rendering of the {c}.",
+    lambda c: f"a {c} in a video game.",
+    lambda c: f"a photo of one {c}.",
+    lambda c: f"a doodle of a {c}.",
+    lambda c: f"a close-up photo of the {c}.",
+    lambda c: f"a photo of a {c}.",
+    lambda c: f"the origami {c}.",
+    lambda c: f"the {c} in a video game.",
+    lambda c: f"a sketch of a {c}.",
+    lambda c: f"a doodle of the {c}.",
+    lambda c: f"a origami {c}.",
+    lambda c: f"a low resolution photo of a {c}.",
+    lambda c: f"the toy {c}.",
+    lambda c: f"a rendition of the {c}.",
+    lambda c: f"a photo of the clean {c}.",
+    lambda c: f"a photo of a large {c}.",
+    lambda c: f"a rendition of a {c}.",
+    lambda c: f"a photo of a nice {c}.",
+    lambda c: f"a photo of a weird {c}.",
+    lambda c: f"a blurry photo of a {c}.",
+    lambda c: f"a cartoon {c}.",
+    lambda c: f"art of a {c}.",
+    lambda c: f"a sketch of the {c}.",
+    lambda c: f"a embroidered {c}.",
+    lambda c: f"a pixelated photo of a {c}.",
+    lambda c: f"itap of the {c}.",
+    lambda c: f"a jpeg corrupted photo of the {c}.",
+    lambda c: f"a good photo of a {c}.",
+    lambda c: f"a plushie {c}.",
+    lambda c: f"a photo of the nice {c}.",
+    lambda c: f"a photo of the small {c}.",
+    lambda c: f"a photo of the weird {c}.",
+    lambda c: f"the cartoon {c}.",
+    lambda c: f"art of the {c}.",
+    lambda c: f"a drawing of the {c}.",
+    lambda c: f"a photo of the large {c}.",
+    lambda c: f"a black and white photo of a {c}.",
+    lambda c: f"the plushie {c}.",
+    lambda c: f"a dark photo of a {c}.",
+    lambda c: f"itap of a {c}.",
+    lambda c: f"graffiti of the {c}.",
+    lambda c: f"a toy {c}.",
+    lambda c: f"itap of my {c}.",
+    lambda c: f"a photo of a cool {c}.",
+    lambda c: f"a photo of a small {c}.",
+    lambda c: f"a tattoo of the {c}.",
+]
