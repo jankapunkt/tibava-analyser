bf69f41d4a7e527c6bf40da8aa613ab6b55cee69
Anis Mekacher <anis@venacus.com>
2024-08-16 08:51:29 +0200

    ✨ Added OCR plugin.

--- data_python/src/plugins/string_data.py
+++ data_python/src/plugins/string_data.py
@@ -37,3 +37,26 @@ class StringData(Data):
 
     def to_dict(self) -> dict:
         return {"text": self.text}
+    
+@DataManager.export("StringsData", analyser_pb2.TEXT_EMBEDDING_DATA)
+@dataclass(kw_only=True)
+class StringsData(Data):
+    type: str = field(default="StringsData")
+    strings: List[StringData] = field(default_factory=list)
+
+    def load(self) -> None:
+        super().load()
+        assert self.check_fs(), "No filesystem handler installed"
+
+        data = self.load_dict("strings_data.yml")
+        self.strings = [StringData(**x) for x in data.get("strings")]
+
+    def save(self) -> None:
+        super().save()
+        assert self.check_fs(), "No filesystem handler installed"
+        assert self.fs.mode == "w", "Data packet is open read only"
+
+        self.save_dict("strings_data.yml", {"strings": [t.to_dict() for t in self.strings]})
+
+    def to_dict(self) -> dict:
+        return {**super().to_dict(), "strings": [t.to_dict() for t in self.strings]}
